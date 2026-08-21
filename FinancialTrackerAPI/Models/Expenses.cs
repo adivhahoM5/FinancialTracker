c7819659072ef67bebf84a5e11a35b0d17ee28fc
ADIVHAHO MULAUDZI <adivhahoM5@outlook.com>
2026-08-21 07:21:53 +0200

    update cs

---
+++ FinancialTrackerAPI/Models/Expenses.cs
@@ -0,0 +1,12 @@
+﻿namespace FinancialTrackerAPI.Models
+{
+    public class Expenses
+    {
+        public string name { get; set; } = string.Empty;
+        public int amount;
+        public string category { get; set; } = string.Empty;
+        public string account { get; set; } = string.Empty;
+        DateTime date { get; set; }
+        public string description { get; set; } = string.Empty;
+    }
+}
