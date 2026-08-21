4f0a9a1cf8a3991fad302b83c554d0fcb8c6391f
ADIVHAHO MULAUDZI <adivhahoM5@outlook.com>
2026-08-21 07:17:18 +0200

    Add cs models
    added Income, Expenses and created cs models for APIs

--- script.js
+++ script.js
@@ -81,8 +81,114 @@ function showExpenses() {
         </div>
     `;
 }
+addEventListener()
+function showAccounts(){
+    document.getElementById("display").innerHTML =`
+    
+      <h3>Accounts</h3>
+      <button class="save-button" onClick="addAccount()">Add Account</button>
+      <button class="save-button" onClick="deleteAccount()">Delete Account</button>
+      <button class="save-button" onClick="updateAccount()">Update Account name</button><br><br>
+      <div class="form-container" id = "currentAccounts">
+        
+        <div class="page-header">
+            <h3>Current Accounts</h3>
+            <table>
 
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Current Balance</th>
+                        
+                    </tr>
+                </thead>
 
+                <tbody>
+
+                    <tr>
+                        <td>Capitec</td>
+                        <td>Savings</td>
+                        <td>R5,000.00</td>
+                        
+                    </tr>
+
+                    <tr>
+                        <td>FNB</td>
+                        <td>Cheque</td>
+                        <td>R8,500.00</td>
+                       
+                    </tr>
+
+                    <tr>
+                        <td>Standard Bank</td>
+                        <td>Savings</td>
+                        <td>R3,200.00</td>
+                        
+                    </tr>
+
+                </tbody>
+
+            </table>
+        </div>
+      </div>
+    `;
+}
+function addAccount(){
+  document.getElementById("currentAccounts").innerHTML = `
+  <div class="page-header">
+            <h3>Add Account</h3> </div> 
+           <label>Account type</label>
+            <select>
+                <option>Cheque</option>
+                <option>Savings</option>
+                <option > Notice</option>
+                <option > Other> </ option>
+               
+            </select><br><br>
+             <label>Balance</label>
+            <input type="number" placeholder="R0.00">
+             <label>Account Name</label>
+            <input type="text" placeholder="e.g. Big Bank">
+            <button class="save-button"> Add to my Account </button>
+
+  `;
+}
+function deleteAccount(){
+  document.getElementById("currentAccounts").innerHTML = `
+  <div class="page-header">
+            <h3>Delete Account</h3> </div> 
+           <label>Account </label>
+            <select>
+                <option>Cheque</option>
+                <option>Savings</option>
+                <option > Notice</option>
+                <option > Other> </ option>
+               
+            </select><br><br>
+             
+            <button class="save-button"> Delete Account </button>
+
+  `;
+}
+function updateAccount(){
+  document.getElementById("currentAccounts").innerHTML = `
+  <div class="page-header">
+            <h3>Update Account Name </h3> </div> 
+           <label>Account </label>
+            <select>
+                <option>Cheque</option>
+                <option>Savings</option>
+                <option > Notice</option>
+                <option > Other> </ option>
+               
+            </select><br><br>
+             <label>Account Name</label>
+            <input type="text" placeholder="e.g. Big Bank">
+            <button class="save-button"> Update Account Name </button>
+
+  `;
+}
 function showTransfer() {
 
     document.getElementById("display").innerHTML = `
